4a937ffe0789f96be17baead16dcb0dfc2aa9ff8
Yicong Eddy Luo <m1662576219@gmail.com>
2025-03-31 19:32:06 -0400

    debug

--- sm90/kernels.cuh
+++ sm90/kernels.cuh
@@ -72,16 +72,19 @@ __global__ void kernel_contiguous(int * data, int size, int work_per_block, int 
 	
 	if (threadIdx.x == 0) {
 		init(&bar, blockDim.x);
+		cde::fence_proxy_async_shared_cta();
 	}
+	
+	__syncthreads();
 	
 	uint64_t token;
 	
 	int sum = 0;
 	
 	for (int i = 0; i < work_per_block/shared_size; ++i) {
-		if (tid == 0) {
+		if (threadIdx.x == 0) {
 			// call the loading api
-			cde::cp_async_bulk_global_to_shared(shared, data + i * work_per_block * BLOCKS_PER_GRID + block_id * work_per_block, sizeof(shared), bar);
+			cde::cp_async_bulk_global_to_shared(shared, data + i * work_per_block * BLOCKS_PER_GRID + block_id * shared_size, sizeof(shared), bar);
 			token = cuda::device::barrier_arrive_tx(bar, 1, sizeof(shared));
 		} else {
 			token = bar.arrive();
@@ -89,49 +92,14 @@ __global__ void kernel_contiguous(int * data, int size, int work_per_block, int 
 		
 		bar.wait(std::move(token));
 		
+		__syncthreads();
+		
 		for (int j = 0; j < BLOCK_SIZE; j++) {
-			sum += shared[thread_id * BLOCK_SIZE + j];
+			sum += shared[threadIdx.x * BLOCK_SIZE + j];
 		}
+		
+		__syncthreads();
 	}
-	
-	results[thread_id] = sum;
-	
-}
-
-__global__ void test(int * data, int *results) {
-	
-	const int warp_id = (threadIdx.x / 32) + (blockIdx.x * WARPS_PER_BLOCK);
-	const int block_id = blockIdx.x;
-	const int tid = threadIdx.x % 32;
-	const int thread_id = threadIdx.x + (blockIdx.x * blockDim.x);
-	
-	const int shared_size = 32 * WARPS_PER_BLOCK * BLOCK_SIZE;
-	
-	__shared__ alignas(16) int shared[64];
-	
-	__shared__ barrier bar;
-	
-	if (threadIdx.x == 0) {
-		init(&bar, blockDim.x);
-		cde::fence_proxy_async_shared_cta();
-	}
-	
-	
-	barrier::arrival_token token;
-	
-	int sum = 0;
-
-	if (threadIdx.x == 0) {
-		// call the loading api
-		cde::cp_async_bulk_global_to_shared(shared, data, sizeof(shared), bar);
-		token = cuda::device::barrier_arrive_tx(bar, 1, sizeof(shared));
-	} else {
-		token = bar.arrive();
-	}
-	
-	bar.wait(std::move(token));
-	
-	sum += shared[0];
 	
 	results[thread_id] = sum;
 	
@@ -139,6 +107,35 @@ __global__ void test(int * data, int *results) {
 
 
 static constexpr size_t buf_len = 1024;
+
+__global__ void test(int * data, int *results) {
+	
+	// Shared memory buffers for x and y. The destination shared memory buffer of
+  // a bulk operations should be 16 byte aligned.
+  __shared__ alignas(16) int smem_data[buf_len];
+
+  // 1. a) Initialize shared memory barrier with the number of threads participating in the barrier.
+  //    b) Make initialized barrier visible in async proxy.
+  #pragma nv_diag_suppress static_var_with_dynamic_init
+  __shared__ barrier bar;
+  if (threadIdx.x == 0) { 
+    init(&bar, blockDim.x);                // a)
+    cde::fence_proxy_async_shared_cta();   // b)
+  }
+  __syncthreads();
+
+  // 2. Initiate TMA transfer to copy global to shared memory.
+  barrier::arrival_token token;
+  if (threadIdx.x == 0) {
+    cde::cp_async_bulk_global_to_shared(smem_data, data + offset, sizeof(smem_data), bar);
+    // 3a. Arrive on the barrier and tell how many bytes are expected to come in (the transaction count)
+    token = cuda::device::barrier_arrive_tx(bar, 1, sizeof(smem_data));
+  } else {
+    // 3b. Rest of threads just arrive
+    token = bar.arrive();
+  }
+	
+}
 
 __global__ void add_one_kernel(int* data, size_t offset)
 {
